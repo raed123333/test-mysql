6b74b71a0190e3b78ddddcd346ee39bc8d5d9049
JABRI Raed <jabriraed80@gmail.com>
2025-02-17 13:17:43 +0100

    run server with nodemon

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+const app=express();
+
+app.use(cors());
+app.listen(8081,()=>{
+        console.log("listening on port 8081");
+})
